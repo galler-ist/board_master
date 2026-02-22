317a7563c56f05f1e7d68ab3022385f0427b26e8
swghj0609 <swghj0609@gmail.com>
2026-02-22 20:24:54 +0000

    fix: Refine Disqus TypeScript types

--- src/types/global.d.ts
+++ src/types/global.d.ts
@@ -1,5 +1,22 @@
 // global.d.ts
+interface DisqusPageConfig {
+  url: string;
+  identifier: string;
+  title: string;
+}
+
 interface Window {
-  disqus_config: any; // Type for disqus_config object
+  disqus_config: () => void; // Declare disqus_config as a function
   DISQUS: any;       // Type for DISQUS object
+}
+
+// Extend the global "this" within the disqus_config function
+declare namespace DisqusJS {
+  interface Config {
+    page: DisqusPageConfig;
+  }
+}
+
+interface Window {
+  disqus_config: (this: DisqusJS.Config) => void;
 }
